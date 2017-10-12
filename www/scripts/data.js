e51b2afae948fe790e037b79d5d6884b85b20ab2
Glenn Jong <zaku0225@gamer.com.tw>
2017-10-12 16:30:26 +0800

    試看看 firefox

--- www/scripts/data.js
+++ www/scripts/data.js
@@ -1,8 +1,8 @@
 // Get Data
 function getData(siteName) {
   $.ajax({
-    // url: "http://opendata2.epa.gov.tw/AQI.json",
-    url: "//opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000260/?format=json&token=H7vuj5zggE2Fw76iGDaEug",
+    url: "http://opendata2.epa.gov.tw/AQI.json",
+    // url: "//opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000260/?format=json&token=H7vuj5zggE2Fw76iGDaEug",
     type: "GET",
     dataType: "json",
     success: function(data) {
